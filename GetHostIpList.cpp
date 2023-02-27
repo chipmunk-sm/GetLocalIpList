d38647e6750e3a0fee8fe8356a0ffa1a33e2ee30
chipmunk.sm <dannico@linuxmail.org>
2023-02-27 12:00:10 +0100

    update

--- GetHostIpList.cpp
+++ GetHostIpList.cpp
@@ -26,8 +26,8 @@ std::string GetWsaErrorString(int errorCode)
 {
 	std::string retv(std::to_string(errorCode) + ": ");
 	std::vector<char> buf(1024, 0);
-	FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, nullptr, static_cast<DWORD>(errorCode),	MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), buf.data(), static_cast<DWORD>(buf.size() - 1), nullptr);
-	for (auto& item : buf)	{
+	FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, nullptr, static_cast<DWORD>(errorCode), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), buf.data(), static_cast<DWORD>(buf.size() - 1), nullptr);
+	for (auto& item : buf) {
 		if (item == '\r' || item == '\n') {
 			item = 0;
 		}
@@ -41,14 +41,11 @@ typedef struct IpItem
 	IpItem(std::wstring ip, std::wstring adapter, ADDRESS_FAMILY family) :
 		Ip(ip),
 		Adapter(adapter),
-		Family(family)
-	{
+		Family(family) {
 	}
 
-	auto GetFamilyString() 
-	{
-		switch (Family)
-		{
+	auto GetFamilyString() {
+		switch (Family) {
 		case AF_UNSPEC: return "Unspecified";
 		case AF_UNIX: return "local to host (pipes, portals)";
 		case AF_INET: return "IPv4";
@@ -93,11 +90,9 @@ std::vector<IpItem> GetLocalIpList()
 	std::vector<IpItem> ipList;
 	std::vector<BYTE> adapterInfo;
 
-	try
-	{
+	try {
 		DWORD size = 0;
-		if (GetAdaptersAddresses(AF_UNSPEC, GAA_FLAG_INCLUDE_PREFIX, NULL, NULL, &size) != ERROR_BUFFER_OVERFLOW)
-		{
+		if (GetAdaptersAddresses(AF_UNSPEC, GAA_FLAG_INCLUDE_PREFIX, NULL, NULL, &size) != ERROR_BUFFER_OVERFLOW) {
 			auto lastErr = WSAGetLastError();
 			auto err = "Failed get_ipaddress - GetAdaptersAddresses get size" + GetWsaErrorString(lastErr);
 			throw std::exception(err.c_str());
@@ -105,22 +100,18 @@ std::vector<IpItem> GetLocalIpList()
 
 		adapterInfo.resize(size);
 
-		if (GetAdaptersAddresses(AF_UNSPEC, GAA_FLAG_INCLUDE_PREFIX, NULL, (PIP_ADAPTER_ADDRESSES)adapterInfo.data(), &size) != ERROR_SUCCESS)
-		{
+		if (GetAdaptersAddresses(AF_UNSPEC, GAA_FLAG_INCLUDE_PREFIX, NULL, (PIP_ADAPTER_ADDRESSES)adapterInfo.data(), &size) != ERROR_SUCCESS) {
 			auto lastErr = WSAGetLastError();
 			auto err = "Failed get_ipaddress - GetAdaptersAddresses get size" + GetWsaErrorString(lastErr);
 			throw std::exception("Failed get_ipaddress - GetAdaptersAddresses get value");
 		}
 
-		for (auto a1 = (PIP_ADAPTER_ADDRESSES)adapterInfo.data(); a1 != NULL; a1 = a1->Next)
-		{
+		for (auto a1 = (PIP_ADAPTER_ADDRESSES)adapterInfo.data(); a1 != NULL; a1 = a1->Next) {
 			std::wstring adapter(a1->FriendlyName);
-			for (auto a2 = a1->FirstUnicastAddress; a2 != NULL; a2 = a2->Next) 
-			{
+			for (auto a2 = a1->FirstUnicastAddress; a2 != NULL; a2 = a2->Next) {
 				std::vector<wchar_t> buf(BUFSIZ + 1, 0);
 				if (GetNameInfoW(a2->Address.lpSockaddr, a2->Address.iSockaddrLength,
-					buf.data(), static_cast<DWORD>(buf.size() - 1), NULL, 0, NI_NUMERICHOST) == 0)
-				{
+					buf.data(), static_cast<DWORD>(buf.size() - 1), NULL, 0, NI_NUMERICHOST) == 0) {
 					ipList.emplace_back(buf.data(), adapter, a2->Address.lpSockaddr->sa_family);
 				}
 			}
@@ -129,21 +120,14 @@ std::vector<IpItem> GetLocalIpList()
 		return ipList;
 
 	}
-	catch (const std::exception& )
-	{
+	catch (const std::exception&) {
 		throw;
 	}
-	catch (...)
-	{
-		try// c++xx >= c++11
-		{
-			std::exception_ptr curr_excp = std::current_exception();
-			if (curr_excp)
-				std::rethrow_exception(curr_excp);
-			throw std::runtime_error("Unexpected exception");
+	catch (...) {
+		try {
+			std::rethrow_exception(std::current_exception());
 		}
-		catch (const std::exception& )
-		{
+		catch (...) {
 			throw;
 		}
 	}
@@ -170,8 +154,7 @@ int main(int argc, char* argv[])
 		size_t adapterLen = 0;
 		size_t familyLen = 0;
 
-		for (auto& item : addresList)
-		{
+		for (auto& item : addresList) {
 			ipLen = std::max<decltype(ipLen)>(ipLen, static_cast<decltype(ipLen)>(item.Ip.length()));
 			adapterLen = std::max<decltype(adapterLen)>(adapterLen, static_cast<decltype(adapterLen)>(item.Adapter.length()));
 			familyLen = std::max<decltype(familyLen)>(familyLen, static_cast<decltype(familyLen)>(lstrlenA(item.GetFamilyString())));
@@ -179,8 +162,7 @@ int main(int argc, char* argv[])
 
 		wprintf(L"\n--- START ---\n");
 
-		for (auto& item : addresList)
-		{
+		for (auto& item : addresList) {
 			wprintf(L"IP [%s] %s %S %s %s %s;\n",
 				item.Ip.c_str(),
 				std::wstring(ipLen - static_cast<decltype(ipLen)>(item.Ip.length()), ' ').c_str(),
@@ -194,21 +176,14 @@ int main(int argc, char* argv[])
 		wprintf(L"--- END ---\n");
 
 	}
-	catch (const std::exception& ex)
-	{
+	catch (const std::exception& ex) {
 		printf("FATAL: %s", ex.what());
 	}
-	catch (...)
-	{
-		try// c++xx >= c++11
-		{
-			std::exception_ptr curr_excp = std::current_exception();
-			if (curr_excp)
-				std::rethrow_exception(curr_excp);
-			throw std::runtime_error("Failed get_ipaddress - Unexpected exception");
+	catch (...) {
+		try {
+			std::rethrow_exception(std::current_exception());
 		}
-		catch (const std::exception& ex)
-		{
+		catch (const std::exception& ex) {
 			printf("FATAL: %s", ex.what());
 		}
 	}
